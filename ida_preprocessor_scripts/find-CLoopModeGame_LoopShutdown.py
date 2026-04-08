59b777c996eba8e05c78e190b0c2fe78c721f4f5
hzqst <113660872@qq.com>
2026-04-08 22:11:14 +0800

    Fix https://github.com/HLND2T/CS2_VibeSignatures/issues/217

--- ida_preprocessor_scripts/find-CLoopModeGame_LoopShutdown.py
+++ ida_preprocessor_scripts/find-CLoopModeGame_LoopShutdown.py
@@ -3,17 +3,39 @@
 
 from ida_analyze_util import preprocess_common_skill
 
-TARGET_FUNCTION_NAMES = [
+# CLoopModeGame_Shutdown has been inlined into CLoopModeGame_LoopShutdown
+TARGET_FUNCTION_NAMES_WINDOWS = [
     "CLoopModeGame_LoopShutdown",
 ]
 
-FUNC_XREFS = [
+TARGET_FUNCTION_NAMES_LINUX = [
+    "CLoopModeGame_Shutdown",
+    "CLoopModeGame_LoopShutdown",
+]
+
+FUNC_XREFS_WINDOWS = [
     # (func_name, xref_strings_list, xref_funcs_list, exclude_funcs_list)
     (
         "CLoopModeGame_LoopShutdown",
         ["--CLoopModeGame::SetWorldSession"],
-        [],
+        ["CLoopModeGame_SetGameSystemState", "IGameSystem_DestroyAllGameSystems"],
         ["CLoopModeGame_ReceivedServerInfo", "CLoopModeGame_SetWorldSession"],
+    ),
+]
+
+FUNC_XREFS_LINUX = [
+    # (func_name, xref_strings_list, xref_funcs_list, exclude_funcs_list)
+    (
+        "CLoopModeGame_Shutdown",
+        ["--CLoopModeGame::SetWorldSession"],
+        ["CLoopModeGame_SetGameSystemState", "IGameSystem_DestroyAllGameSystems"],
+        ["CLoopModeGame_SetWorldSession"],
+    ),
+    (
+        "CLoopModeGame_LoopShutdown",
+        [],
+        ["CLoopModeGame_Shutdown"],
+        ["CLoopModeGame_SetWorldSession"],
     ),
 ]
 
@@ -35,8 +57,8 @@ async def preprocess_skill(
         new_binary_dir=new_binary_dir,
         platform=platform,
         image_base=image_base,
-        func_names=TARGET_FUNCTION_NAMES,
-        func_xrefs=FUNC_XREFS,
+        func_names=TARGET_FUNCTION_NAMES_WINDOWS if platform == "windows" else TARGET_FUNCTION_NAMES_LINUX,
+        func_xrefs=FUNC_XREFS_WINDOWS if platform == "windows" else FUNC_XREFS_LINUX,
         func_vtable_relations=FUNC_VTABLE_RELATIONS,
         debug=debug,
     )
