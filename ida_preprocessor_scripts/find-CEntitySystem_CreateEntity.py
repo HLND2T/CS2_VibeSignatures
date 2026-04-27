e885ae86478de1cbad04bc6da9a661dcf324f697
hzqst <113660872@qq.com>
2026-04-27 11:59:45 +0800

    Add find-CEntitySystem_CreateEntity preprocessor script

---
+++ ida_preprocessor_scripts/find-CEntitySystem_CreateEntity.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""Preprocess script for find-CEntitySystem_CreateEntity skill."""
+
+from ida_analyze_util import preprocess_common_skill
+
+TARGET_FUNCTION_NAMES = [
+    "CEntitySystem_CreateEntity",
+]
+
+FUNC_XREFS = [
+    {
+        "func_name": "CEntitySystem_CreateEntity",
+        "xref_strings": [
+            "Cannot create an entity because entity class is NULL %d",
+            "Cannot create a %s entity because the class has limited schema metadata",
+        ],
+        "xref_gvs": [],
+        "xref_signatures": [],
+        "xref_funcs": [],
+        "exclude_funcs": [],
+        "exclude_strings": [],
+        "exclude_gvs": [],
+        "exclude_signatures": [],
+    },
+]
+
+GENERATE_YAML_DESIRED_FIELDS = [
+    # (symbol_name, generate_yaml_fields)
+    (
+        "CEntitySystem_CreateEntity",
+        [
+            "func_name",
+            "func_sig",
+            "func_va",
+            "func_rva",
+            "func_size",
+        ],
+    ),
+]
+
+async def preprocess_skill(
+    session, skill_name, expected_outputs, old_yaml_map,
+    new_binary_dir, platform, image_base, debug=False,
+):
+    """Reuse previous gamever func_sig to locate target function(s) and write YAML."""
+    return await preprocess_common_skill(
+        session=session,
+        expected_outputs=expected_outputs,
+        old_yaml_map=old_yaml_map,
+        new_binary_dir=new_binary_dir,
+        platform=platform,
+        image_base=image_base,
+        func_names=TARGET_FUNCTION_NAMES,
+        func_xrefs=FUNC_XREFS,
+        generate_yaml_desired_fields=GENERATE_YAML_DESIRED_FIELDS,
+        debug=debug,
+    )
