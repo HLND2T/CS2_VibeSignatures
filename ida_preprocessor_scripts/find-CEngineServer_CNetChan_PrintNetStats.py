8b2933aeedac37a446badb86b15800dc78a933f1
Jerry.J.Jiang <jerry.j.jiang@benq.com>
2026-06-29 16:19:06 +0800

    Add find-CEngineServer_DumpNetStats preprocessor scripts.
    - find-CEngineServer_CNetChan_PrintNetStats: Pattern A with platform-
      specific xref_signatures (Windows: 48 8B C4 55 53 56..., Linux:
      55 B9 01 00 00 00 BA FF 00 00 00 48 89 E5); finds the copy of
      CNetChan_PrintNetStats compiled into engine2.dll / libengine2.so
    - find-CEngineServer_DumpNetStats: Pattern B using xref_funcs on
      CEngineServer_CNetChan_PrintNetStats; output includes vtable_name,
      vfunc_offset, vfunc_index (entry 29 of CEngineServer_vtable)

---
+++ ida_preprocessor_scripts/find-CEngineServer_CNetChan_PrintNetStats.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+"""Preprocess script for find-CEngineServer_CNetChan_PrintNetStats skill."""
+
+from ida_analyze_util import preprocess_common_skill
+
+TARGET_FUNCTION_NAMES = [
+    "CEngineServer_CNetChan_PrintNetStats",
+]
+
+FUNC_XREFS_WINDOWS = [
+    {
+        "func_name": "CEngineServer_CNetChan_PrintNetStats",
+        "xref_strings": [],
+        "xref_gvs": [],
+        "xref_signatures": [
+            "48 8B C4 55 53 56 48 8D A8 ?? ?? ?? ??",
+        ],
+        "xref_funcs": [],
+        "exclude_funcs": [],
+        "exclude_strings": [],
+        "exclude_gvs": [],
+        "exclude_signatures": [],
+    },
+]
+
+FUNC_XREFS_LINUX = [
+    {
+        "func_name": "CEngineServer_CNetChan_PrintNetStats",
+        "xref_strings": [],
+        "xref_gvs": [],
+        "xref_signatures": [
+            "55 B9 01 00 00 00 BA FF 00 00 00 48 89 E5",
+        ],
+        "xref_funcs": [],
+        "exclude_funcs": [],
+        "exclude_strings": [],
+        "exclude_gvs": [],
+        "exclude_signatures": [],
+    },
+]
+
+GENERATE_YAML_DESIRED_FIELDS = [
+    # (symbol_name, generate_yaml_fields)
+    (
+        "CEngineServer_CNetChan_PrintNetStats",
+        [
+            "func_name",
+            "func_sig",
+            "func_va",
+            "func_rva",
+            "func_size",
+        ],
+    ),
+]
+
+async def preprocess_skill(
+    session, skill_name, expected_outputs, old_yaml_map,
+    new_binary_dir, platform, image_base, debug=False,
+):
+    """Reuse previous gamever func_sig to locate target function(s) and write YAML."""
+    return await preprocess_common_skill(
+        session=session,
+        expected_outputs=expected_outputs,
+        old_yaml_map=old_yaml_map,
+        new_binary_dir=new_binary_dir,
+        platform=platform,
+        image_base=image_base,
+        func_names=TARGET_FUNCTION_NAMES,
+        func_xrefs=FUNC_XREFS_WINDOWS if platform == "windows" else FUNC_XREFS_LINUX,
+        generate_yaml_desired_fields=GENERATE_YAML_DESIRED_FIELDS,
+        debug=debug,
+    )
