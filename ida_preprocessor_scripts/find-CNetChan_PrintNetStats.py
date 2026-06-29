fd90d58402ade8ac6b3edd91bc3b660b59a1b372
Jerry.J.Jiang <jerry.j.jiang@benq.com>
2026-06-29 15:09:47 +0800

    Add find-CNetChan_PrintNetStats preprocessor script.
    Windows uses xref_strings on "Bandwidth . . . . . . . : Total:%.1fkb";
    Linux uses xref_signatures on the function prologue bytes since the
    Bandwidth string doesn't exist in the Linux binary.

---
+++ ida_preprocessor_scripts/find-CNetChan_PrintNetStats.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+"""Preprocess script for find-CNetChan_PrintNetStats skill."""
+
+from ida_analyze_util import preprocess_common_skill
+
+TARGET_FUNCTION_NAMES = [
+    "CNetChan_PrintNetStats",
+]
+
+FUNC_XREFS_WINDOWS = [
+    {
+        "func_name": "CNetChan_PrintNetStats",
+        "xref_strings": [
+            "Bandwidth . . . . . . . : Total:%.1fkb",
+        ],
+        "xref_gvs": [],
+        "xref_signatures": [],
+        "xref_funcs": [],
+        "exclude_funcs": [],
+        "exclude_strings": [],
+        "exclude_gvs": [],
+        "exclude_signatures": [],
+    },
+]
+
+FUNC_XREFS_LINUX = [
+    {
+        "func_name": "CNetChan_PrintNetStats",
+        "xref_strings": [],
+        "xref_gvs": [],
+        "xref_signatures": [
+            "55 31 C9 BA ?? ?? ?? ?? BE ?? ?? ?? ?? 48 89 E5",
+        ],
+        "xref_funcs": [],
+        "exclude_funcs": [],
+        "exclude_strings": [],
+        "exclude_gvs": [],
+        "exclude_signatures": [],
+    },
+]
+
+GENERATE_YAML_DESIRED_FIELDS = [
+    # (symbol_name, generate_yaml_fields)
+    (
+        "CNetChan_PrintNetStats",
+        [
+            "func_name",
+            "func_sig",
+            "func_va",
+            "func_rva",
+            "func_size",
+        ],
+    ),
+]
+
+async def preprocess_skill(
+    session, skill_name, expected_outputs, old_yaml_map,
+    new_binary_dir, platform, image_base, debug=False,
+):
+    """Reuse previous gamever func_sig to locate target function(s) and write YAML."""
+    return await preprocess_common_skill(
+        session=session,
+        expected_outputs=expected_outputs,
+        old_yaml_map=old_yaml_map,
+        new_binary_dir=new_binary_dir,
+        platform=platform,
+        image_base=image_base,
+        func_names=TARGET_FUNCTION_NAMES,
+        func_xrefs=FUNC_XREFS_WINDOWS if platform == "windows" else FUNC_XREFS_LINUX,
+        generate_yaml_desired_fields=GENERATE_YAML_DESIRED_FIELDS,
+        debug=debug,
+    )
