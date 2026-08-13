465197ddeccadbcd8b476914a7e687a673b12a35
Jerry.J.Jiang <jerry.j.jiang@benq.com>
2026-08-13 16:56:04 +0800

    Add CBasePlayerPawn destructor preprocessor

---
+++ ida_preprocessor_scripts/find-CBasePlayerPawn_dtor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Preprocess script for find-CBasePlayerPawn_dtor skill."""
+from ida_analyze_util import preprocess_common_skill
+
+TARGET_FUNCTION_NAMES = ["CBasePlayerPawn_dtor"]
+FUNC_XREFS = [{"func_name": "CBasePlayerPawn_dtor", "xref_strings": [], "xref_gvs": ["CBasePlayerPawn_vtable"], "xref_signatures": [], "xref_funcs": [], "exclude_funcs": [], "exclude_strings": [], "exclude_gvs": [], "exclude_signatures": []}]
+FUNC_VTABLE_RELATIONS = [("CBasePlayerPawn_dtor", "CBasePlayerPawn")]
+GENERATE_YAML_DESIRED_FIELDS = [("CBasePlayerPawn_dtor", ["func_name", "func_va", "func_rva", "func_size", "func_sig", "vtable_name", "vfunc_offset", "vfunc_index"])]
+
+async def preprocess_skill(session, skill_name, expected_outputs, old_yaml_map, new_binary_dir, platform, image_base, debug=False):
+    _ = skill_name
+    return await preprocess_common_skill(session=session, expected_outputs=expected_outputs, old_yaml_map=old_yaml_map, new_binary_dir=new_binary_dir, platform=platform, image_base=image_base, func_names=TARGET_FUNCTION_NAMES, func_xrefs=FUNC_XREFS, func_vtable_relations=FUNC_VTABLE_RELATIONS, generate_yaml_desired_fields=GENERATE_YAML_DESIRED_FIELDS, debug=debug)
