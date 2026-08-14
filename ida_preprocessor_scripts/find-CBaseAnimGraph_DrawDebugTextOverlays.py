be0c8146cc55ff8d086218451b5a62d253d313ea
Jerry.J.Jiang <jerry.j.jiang@benq.com>
2026-08-14 09:34:03 +0800

    fix(preprocessor): use CBaseAnimGraph vtable relation

--- ida_preprocessor_scripts/find-CBaseAnimGraph_DrawDebugTextOverlays.py
+++ ida_preprocessor_scripts/find-CBaseAnimGraph_DrawDebugTextOverlays.py
@@ -3,7 +3,7 @@
 from ida_analyze_util import preprocess_common_skill
 TARGET_FUNCTION_NAMES = ["CBaseAnimGraph_DrawDebugTextOverlays"]
 FUNC_XREFS = [{"func_name": "CBaseAnimGraph_DrawDebugTextOverlays", "xref_strings": ["FULLMATCH:AnimGraph2: %s"], "xref_gvs": [], "xref_signatures": [], "xref_funcs": [], "exclude_funcs": [], "exclude_strings": [], "exclude_gvs": [], "exclude_signatures": []}]
-FUNC_VTABLE_RELATIONS = [("CBaseAnimGraph_DrawDebugTextOverlays", "CBaseAnimGraph")]
+FUNC_VTABLE_RELATIONS = [("CBaseAnimGraph_DrawDebugTextOverlays", "CBaseAnimGraph_vtable")]
 GENERATE_YAML_DESIRED_FIELDS = [("CBaseAnimGraph_DrawDebugTextOverlays", ["func_name", "func_va", "func_rva", "func_size", "func_sig", "vtable_name", "vfunc_offset", "vfunc_index"])]
 async def preprocess_skill(session, skill_name, expected_outputs, old_yaml_map, new_binary_dir, platform, image_base, debug=False):
     _ = skill_name
