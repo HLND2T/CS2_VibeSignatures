cb07f2045700292c19d8d3ec2b5f8b7063a30d68
Jerry.J.Jiang <jerry.j.jiang@benq.com>
2026-08-14 12:24:24 +0800

    feat(preprocessor): add CBaseAnimating Dissolve

---
+++ ida_preprocessor_scripts/find-CBaseAnimating_vtable.py
@@ -0,0 +1,31 @@
+from ida_analyze_util import preprocess_common_skill
+
+TARGET_CLASS_NAMES = ["CBaseAnimating"]
+GENERATE_YAML_DESIRED_FIELDS = [
+    (
+        "CBaseAnimating",
+        [
+            "vtable_class",
+            "vtable_symbol",
+            "vtable_va",
+            "vtable_rva",
+            "vtable_size",
+            "vtable_numvfunc",
+            "vtable_entries",
+        ],
+    )
+]
+
+
+async def preprocess_skill(
+    session, skill_name, expected_outputs, old_yaml_map, new_binary_dir, platform, image_base, debug=False
+):
+    return await preprocess_common_skill(
+        session=session,
+        expected_outputs=expected_outputs,
+        vtable_class_names=TARGET_CLASS_NAMES,
+        platform=platform,
+        image_base=image_base,
+        generate_yaml_desired_fields=GENERATE_YAML_DESIRED_FIELDS,
+        debug=debug,
+    )
