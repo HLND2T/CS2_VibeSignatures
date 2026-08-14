99b22bba0ccdb889e7ca282c2e7cd1ef6db146e2
Jerry.J.Jiang <jerry.j.jiang@benq.com>
2026-08-14 09:43:17 +0800

    fix(preprocessor): use Disconnect vtable relations

--- ida_preprocessor_scripts/find-CBasePlayerPawn_Disconnect.py
+++ ida_preprocessor_scripts/find-CBasePlayerPawn_Disconnect.py
@@ -3,7 +3,7 @@
 from ida_analyze_util import preprocess_common_skill
 TARGET_FUNCTION_NAMES = ["CBasePlayerPawn_Disconnect"]
 FUNC_XREFS = [{"func_name": "CBasePlayerPawn_Disconnect", "xref_strings": [], "xref_gvs": [], "xref_signatures": [], "xref_funcs": ["CRagdollProp_Disconnect"], "exclude_funcs": [], "exclude_strings": [], "exclude_gvs": [], "exclude_signatures": []}]
-FUNC_VTABLE_RELATIONS = [("CBasePlayerPawn_Disconnect", "CBasePlayerPawn")]
+FUNC_VTABLE_RELATIONS = [("CBasePlayerPawn_Disconnect", "CBasePlayerPawn_vtable")]
 GENERATE_YAML_DESIRED_FIELDS = [("CBasePlayerPawn_Disconnect", ["func_name", "func_va", "func_rva", "func_size", "func_sig", "vtable_name", "vfunc_offset", "vfunc_index"])]
 async def preprocess_skill(session, skill_name, expected_outputs, old_yaml_map, new_binary_dir, platform, image_base, debug=False):
     _ = skill_name
