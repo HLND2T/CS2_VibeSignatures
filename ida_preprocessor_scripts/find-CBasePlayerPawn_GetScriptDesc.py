f024430d9f66ef0052452f2d64f90fc2da65fe3d
Jerry.J.Jiang <jerry.j.jiang@benq.com>
2026-08-14 09:30:52 +0800

    fix(preprocessor): use CBasePlayerPawn vtable relation

--- ida_preprocessor_scripts/find-CBasePlayerPawn_GetScriptDesc.py
+++ ida_preprocessor_scripts/find-CBasePlayerPawn_GetScriptDesc.py
@@ -4,7 +4,7 @@ from ida_analyze_util import preprocess_common_skill
 
 TARGET_FUNCTION_NAMES = ["CBasePlayerPawn_GetScriptDesc"]
 FUNC_XREFS = [{"func_name": "CBasePlayerPawn_GetScriptDesc", "xref_strings": [], "xref_gvs": [], "xref_signatures": [], "xref_funcs": ["GetScriptDesc"], "exclude_funcs": [], "exclude_strings": [], "exclude_gvs": [], "exclude_signatures": []}]
-FUNC_VTABLE_RELATIONS = [("CBasePlayerPawn_GetScriptDesc", "CBasePlayerPawn")]
+FUNC_VTABLE_RELATIONS = [("CBasePlayerPawn_GetScriptDesc", "CBasePlayerPawn_vtable")]
 GENERATE_YAML_DESIRED_FIELDS = [("CBasePlayerPawn_GetScriptDesc", ["func_name", "func_va", "func_rva", "func_size", "func_sig", "vtable_name", "vfunc_offset", "vfunc_index"])]
 
 async def preprocess_skill(session, skill_name, expected_outputs, old_yaml_map, new_binary_dir, platform, image_base, debug=False):
