61f22c821549d05326cf0e55491fea36752975fb
Jerry.J.Jiang <jerry.j.jiang@benq.com>
2026-08-14 09:18:45 +0800

    fix(preprocessor): use Precache vtable relations

--- ida_preprocessor_scripts/find-CBaseAnimGraph_Precache.py
+++ ida_preprocessor_scripts/find-CBaseAnimGraph_Precache.py
@@ -3,7 +3,7 @@
 from ida_analyze_util import preprocess_common_skill
 TARGET_FUNCTION_NAMES = ["CBaseAnimGraph_Precache"]
 FUNC_XREFS = [{"func_name": "CBaseAnimGraph_Precache", "xref_strings": [], "xref_gvs": [], "xref_signatures": [], "xref_funcs": ["CBaseEntity_Precache"], "exclude_funcs": [], "exclude_strings": [], "exclude_gvs": [], "exclude_signatures": []}]
-FUNC_VTABLE_RELATIONS = [("CBaseAnimGraph_Precache", "CBaseAnimGraph")]
+FUNC_VTABLE_RELATIONS = [("CBaseAnimGraph_Precache", "CBaseAnimGraph_vtable")]
 GENERATE_YAML_DESIRED_FIELDS = [("CBaseAnimGraph_Precache", ["func_name", "func_va", "func_rva", "func_size", "func_sig", "vtable_name", "vfunc_offset", "vfunc_index"])]
 async def preprocess_skill(session, skill_name, expected_outputs, old_yaml_map, new_binary_dir, platform, image_base, debug=False):
     _ = skill_name
