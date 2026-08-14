57eef3df819434b4bc83866703221a445ce0b1dd
Jerry.J.Jiang <jerry.j.jiang@benq.com>
2026-08-14 08:38:16 +0800

    fix(preprocessor): use NetworkServerService vtable relation

--- ida_preprocessor_scripts/find-CNetworkServerService_IsActiveInGame.py
+++ ida_preprocessor_scripts/find-CNetworkServerService_IsActiveInGame.py
@@ -23,7 +23,7 @@ LLM_DECOMPILE = [
 
 FUNC_VTABLE_RELATIONS = [
     # (func_name, vtable_class)
-    ("CNetworkServerService_IsActiveInGame", "CNetworkServerService"),
+    ("CNetworkServerService_IsActiveInGame", "CNetworkServerService_vtable"),
 ]
 
 GENERATE_YAML_DESIRED_FIELDS = [
