43710b77e4b9eaeda144d9136b44e750e40fe44f
Jerry.J.Jiang <jerry.j.jiang@benq.com>
2026-08-14 09:26:54 +0800

    fix(preprocessor): use OnRestore vtable relations

--- ida_preprocessor_scripts/find-CBasePlayerPawn_OnRestore.py
+++ ida_preprocessor_scripts/find-CBasePlayerPawn_OnRestore.py
@@ -3,7 +3,7 @@
 from ida_analyze_util import preprocess_common_skill
 TARGET_FUNCTION_NAMES = ["CBasePlayerPawn_OnRestore"]
 FUNC_XREFS = [{"func_name": "CBasePlayerPawn_OnRestore", "xref_strings": [], "xref_gvs": [], "xref_signatures": [], "xref_funcs": ["CBaseAnimGraph_OnRestore"], "exclude_funcs": [], "exclude_strings": [], "exclude_gvs": [], "exclude_signatures": []}]
-FUNC_VTABLE_RELATIONS = [("CBasePlayerPawn_OnRestore", "CBasePlayerPawn")]
+FUNC_VTABLE_RELATIONS = [("CBasePlayerPawn_OnRestore", "CBasePlayerPawn_vtable")]
 GENERATE_YAML_DESIRED_FIELDS = [("CBasePlayerPawn_OnRestore", ["func_name", "func_va", "func_rva", "func_size", "func_sig", "vtable_name", "vfunc_offset", "vfunc_index"])]
 async def preprocess_skill(session, skill_name, expected_outputs, old_yaml_map, new_binary_dir, platform, image_base, debug=False):
     _ = skill_name
