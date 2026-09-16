8c41bb168c8da3c1b67e2bf90ea6abd165fac741
Jerry.J.Jiang <jerry.j.jiang@benq.com>
2026-09-16 15:00:29 +0800

    style(preprocessor): format Steam networking finders

--- ida_preprocessor_scripts/find-CConnectGameClient_CheckForResend.py
+++ ida_preprocessor_scripts/find-CConnectGameClient_CheckForResend.py
@@ -4,9 +4,37 @@
 from ida_analyze_util import preprocess_common_skill
 
 TARGET_FUNCTION_NAMES = ["CConnectGameClient_CheckForResend"]
-FUNC_XREFS = [{"func_name": "CConnectGameClient_CheckForResend", "xref_strings": ["Sending connect to %s\n"], "xref_gvs": [], "xref_signatures": [], "xref_funcs": [], "exclude_funcs": [], "exclude_strings": [], "exclude_gvs": [], "exclude_signatures": []}]
-GENERATE_YAML_DESIRED_FIELDS = [("CConnectGameClient_CheckForResend", ["func_name", "func_sig", "func_va", "func_rva", "func_size"])]
+FUNC_XREFS = [
+    {
+        "func_name": "CConnectGameClient_CheckForResend",
+        "xref_strings": ["Sending connect to %s\n"],
+        "xref_gvs": [],
+        "xref_signatures": [],
+        "xref_funcs": [],
+        "exclude_funcs": [],
+        "exclude_strings": [],
+        "exclude_gvs": [],
+        "exclude_signatures": [],
+    }
+]
+GENERATE_YAML_DESIRED_FIELDS = [
+    ("CConnectGameClient_CheckForResend", ["func_name", "func_sig", "func_va", "func_rva", "func_size"])
+]
 
-async def preprocess_skill(session, skill_name, expected_outputs, old_yaml_map, new_binary_dir, platform, image_base, debug=False):
+
+async def preprocess_skill(
+    session, skill_name, expected_outputs, old_yaml_map, new_binary_dir, platform, image_base, debug=False
+):
     _ = skill_name
-    return await preprocess_common_skill(session=session, expected_outputs=expected_outputs, old_yaml_map=old_yaml_map, new_binary_dir=new_binary_dir, platform=platform, image_base=image_base, func_names=TARGET_FUNCTION_NAMES, func_xrefs=FUNC_XREFS, generate_yaml_desired_fields=GENERATE_YAML_DESIRED_FIELDS, debug=debug)
+    return await preprocess_common_skill(
+        session=session,
+        expected_outputs=expected_outputs,
+        old_yaml_map=old_yaml_map,
+        new_binary_dir=new_binary_dir,
+        platform=platform,
+        image_base=image_base,
+        func_names=TARGET_FUNCTION_NAMES,
+        func_xrefs=FUNC_XREFS,
+        generate_yaml_desired_fields=GENERATE_YAML_DESIRED_FIELDS,
+        debug=debug,
+    )
