fad5f5cf3cbe70f807dd52a45564b0ab4ceedbbf
Jerry.J.Jiang <jerry.j.jiang@benq.com>
2026-09-17 08:48:23 +0800

    Format GetNetMessageInfo preprocessor

--- ida_preprocessor_scripts/find-CNetworkMessages_GetNetMessageInfo.py
+++ ida_preprocessor_scripts/find-CNetworkMessages_GetNetMessageInfo.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python3
 from ida_analyze_util import preprocess_common_skill
-INHERIT_VFUNCS=[("CNetworkMessages_GetNetMessageInfo","CNetworkMessages","../engine/INetworkMessages_GetNetMessageInfo",True)]
-GENERATE_YAML_DESIRED_FIELDS=[("CNetworkMessages_GetNetMessageInfo",["func_name","func_va","func_rva","func_size","func_sig","vtable_name","vfunc_offset","vfunc_index"])]
-async def preprocess_skill(session,skill_name,expected_outputs,old_yaml_map,new_binary_dir,platform,image_base,debug=False):
-    _=skill_name
-    return await preprocess_common_skill(session=session,expected_outputs=expected_outputs,old_yaml_map=old_yaml_map,new_binary_dir=new_binary_dir,platform=platform,image_base=image_base,inherit_vfuncs=INHERIT_VFUNCS,generate_yaml_desired_fields=GENERATE_YAML_DESIRED_FIELDS,debug=debug)
+
+INHERIT_VFUNCS = [
+    ("CNetworkMessages_GetNetMessageInfo", "CNetworkMessages", "../engine/INetworkMessages_GetNetMessageInfo", True)
+]
+GENERATE_YAML_DESIRED_FIELDS = [
+    (
+        "CNetworkMessages_GetNetMessageInfo",
+        ["func_name", "func_va", "func_rva", "func_size", "func_sig", "vtable_name", "vfunc_offset", "vfunc_index"],
+    )
+]
+
+
+async def preprocess_skill(
+    session, skill_name, expected_outputs, old_yaml_map, new_binary_dir, platform, image_base, debug=False
+):
+    _ = skill_name
+    return await preprocess_common_skill(
+        session=session,
+        expected_outputs=expected_outputs,
+        old_yaml_map=old_yaml_map,
+        new_binary_dir=new_binary_dir,
+        platform=platform,
+        image_base=image_base,
+        inherit_vfuncs=INHERIT_VFUNCS,
+        generate_yaml_desired_fields=GENERATE_YAML_DESIRED_FIELDS,
+        debug=debug,
+    )
